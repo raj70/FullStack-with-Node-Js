d9a591982f139d56f388f61aba5ea271bbe1e1a2
rajen shrestha <rajenshrestha@hotmail.com>
2019-03-28 16:27:03 +1100

    v1.2: converted static react component to class or dynmic react component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import Header from "./Header";
 
-const App = (props) => {
-    return (
-        <div className="App">
-            <Header message="Naming Contests" />
-            <div>
-                ...
+class App extends React.Component {
+    state = {
+        pageHeader: "Naming Contests",
+    };
+
+    componentDidMount() {
+        //console.log("Mounted");
+       // debugger;
+    };
+
+    componentWillUnmount() {
+        //console.log("will unmount");
+       // debugger;
+    };
+
+    render() {
+        return (
+            <div className="App" >
+                <Header message={this.state.pageHeader} />
+                <div>
+                    ...
             </div>
-        </div>
-    );
-};
+            </div>
+        );
+    };
+}
 
 export default App;
