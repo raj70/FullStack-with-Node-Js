0237335446b77e113e9b48646ebc617587ec574e
Rajen Shrestha <rajenshrestha@hotmail.com>
2019-03-29 22:06:28 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from "./components/App";
 ReactDOM.render(    
+    /* the initialdata is made by serverRender.js */
     <App initialContests={window.initialData} />,
     document.getElementById("root")
 );
